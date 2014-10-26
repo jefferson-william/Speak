b7d78a2d057a6625687b55440c657618cfa9ce2e
Jefferson William <jeffersonwilliammachado@gmail.com>
2014-10-25 23:38:29 -0200

    Controlando as sessoes de usuarios e aplicando classes do Bootstrap

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(cookieParser('Speak'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 load('models')
