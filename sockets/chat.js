e175aa91221e1dd5aefdc3543bb976b04bd68565
Jefferson William <jeffersonwilliammachado@gmail.com>
2014-11-01 02:09:39 -0200

    Notificadores na agenda de contatos

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,9 +1,15 @@
 module.exports = function (io) {
 	var crypto = require('crypto');
 	var sockets = io.sockets;
+	var onlines = {};
 	sockets.on('connection', function (client) {
 		var session = client.handshake.session;
 		var usuario = session.usuario;
+		onlines[usuario.email] = usuario.email;
+		for (var email in onlines) {
+			client.emit('notify-onlines', email);
+			client.broadcast.emit('notify-onlines', email);
+		}
 		client.on('join', function (sala) {
 			if (!sala) {
 				var timestamp = new Date().toString();
@@ -24,7 +30,12 @@ module.exports = function (io) {
 			sockets.in(sala).emit('send-client', mensagem);
 		});
 		client.on('disconnect', function () {
-			client.leave(session.sala);
+			var sala = session.sala;
+			var mensagem = '<b>' + usuario.nome + ':</b> saiu.<br>';
+			client.broadcast.emit('notify-offlines', usuario.email);
+			sockets.in(sala).emit('send-client', mensagem);
+			delete onlines[usuario.email];
+			client.leave(sala);
 		});
 	});
 };
