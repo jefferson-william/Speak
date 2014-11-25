a2608eb411717d24c39cf1019a1983526874ecf1
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2014-11-25 01:18:40 -0200

    Aplicando Singleton nas conexões do Mongoose

--- libs/db_connect.js
+++ libs/db_connect.js
@@ -1,7 +1,12 @@
+var mongoose = require('mongoose');
+var config = require('../config.json');
+var env = process.env.NODE_ENV || 'development';
+var url = config.MONGODB[env];
+var single_connection;
+
 module.exports = function () {
-	var mongoose = require('mongoose');
-	var config = require('../config.json');
-	var env = process.env.NODE_ENV || 'development';
-	var url = config.MONGODB[env];
-	return mongoose.connect(url);
+	if (!single_connection) {
+		single_connection = mongoose.connect(url);
+	}
+	return single_connection;
 };
